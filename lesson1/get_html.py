21dc2d26ea62e22965edba25afeb919c14391545
Liberbell <liberbell@gmail.com>
2025-03-19 20:59:00 +0900

    get_html3

--- lesson1/get_html.py
+++ lesson1/get_html.py
@@ -1,6 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://hosh-it.github.io/scraping-practice/"
 response = requests.get(url=url)
 
-print(response.status_code)
+# print(response.status_code)
+# print(response.text)
+
+soup = BeautifulSoup(response.text)
+print(soup.prettify())
