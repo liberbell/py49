3c21b3b010588fb8a130472547c5fd05e41d8161
Liberbell <liberbell@gmail.com>
2025-03-20 21:15:23 +0900

    get_html5

--- lesson1/get_html.py
+++ lesson1/get_html.py
@@ -17,3 +17,8 @@ imgage_tag1 = soup.find("img", class_="size-middle")
 imgage_tags1 = soup.find_all("img", class_="size-middle")
 print(imgage_tag1)
 print(imgage_tags1)
+
+imgage_tag2 = soup.find("img", id="size-small")
+imgage_tags2 = soup.find_all("img", id="size-small")
+print(imgage_tag2)
+print(imgage_tags2)
