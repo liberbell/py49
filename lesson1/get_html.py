ac46896292e13062c5e96707e03d147f228c35e2
Liberbell <liberbell@gmail.com>
2025-03-20 21:30:49 +0900

    get_html9

--- lesson1/get_html.py
+++ lesson1/get_html.py
@@ -15,16 +15,19 @@ image_tags = soup.find_all("img")
 
 imgage_tag1 = soup.find("img", class_="size-middle")
 imgage_tags1 = soup.find_all("img", class_="size-middle")
-print(imgage_tag1)
-print(imgage_tags1)
+# print(imgage_tag1)
+# print(imgage_tags1)
 
 imgage_tag2 = soup.find("img", id="small-size")
 imgage_tags2 = soup.find_all("img", id="small-size")
-print(imgage_tag2)
-print(imgage_tags2)
+# print(imgage_tag2)
+# print(imgage_tags2)
 
-print(imgage_tag2.get("src"))
+# print(imgage_tag2.get("src"))
 
 for i in image_tags:
-    print(i)
+    # print(i)
     print(i["src"])
+print("------")
+for image in image_tags:
+    print(image.get("src"))
