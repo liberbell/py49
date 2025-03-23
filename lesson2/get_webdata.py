86e644abb7cdeb732e104c7f8ee04e2866ef6593
Liberbell <liberbell@gmail.com>
2025-03-24 08:18:23 +0900

    get_webdata3

--- lesson2/get_webdata.py
+++ lesson2/get_webdata.py
@@ -34,11 +34,12 @@ for get_url in a_tags_urls:
     # print("---P2---")
     all_text.append(texts)
 
-    img_tags = sub_soup.find_all("img")
-    img_urls = []
-    for img_tag in img_tags:
-        img_urls.append(img_tag.get("src"))
+img_tags = sub_soup.find_all("img")
+img_urls = []
+for img_tag in img_tags:
+    img_urls.append(img_tag.get("src"))
     print(img_urls)
+print(img_urls)
     # urls.append(url)
     # print(urls)
 # print(all_text)
