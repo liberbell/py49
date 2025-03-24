82551e783423eb988a15869eb7bd4972cfe610f0
Liberbell <liberbell@gmail.com>
2025-03-25 08:29:13 +0900

    get_nikkei2

--- lesson3/get_nikkei.py
+++ lesson3/get_nikkei.py
@@ -3,7 +3,12 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 Home_URL = "https://www.nikkei.com/business/net-media/"
-nikkei_response = requests.get(Home_URL)
-soup = BeautifulSoup(nikkei_response, features="html.parser")
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
+}
+
+nikkei_response = requests.get(Home_URL, headers=headers)
+soup = BeautifulSoup(nikkei_response.text, features="html.parser")
 
 print(soup)
