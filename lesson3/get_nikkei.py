3565dfaa12bc36ffaa32886263bca15991e2ffa5
Liberbell <liberbell@gmail.com>
2025-03-26 08:20:42 +0900

    get_nikkei1

--- lesson3/get_nikkei.py
+++ lesson3/get_nikkei.py
@@ -2,7 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+page = 1
 Home_URL = "https://www.nikkei.com/business/net-media/"
+Get_URL = "https://www.nikkei.com/business/net-media/?page=" + page
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
