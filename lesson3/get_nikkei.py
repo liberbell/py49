5b6099ff8e9c5c7b92c8c8d816d3adef397b74fc
Liberbell <liberbell@gmail.com>
2025-03-28 08:42:58 +0900

    get_nikkei7

--- lesson3/get_nikkei.py
+++ lesson3/get_nikkei.py
@@ -2,55 +2,58 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-page = 1
+page_init = 1
+page_end = 3
 Home_URL = "https://www.nikkei.com/"
-Get_URL = "https://www.nikkei.com/business/net-media/?page=" + str(page)
 
-headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
-}
+for page in range(page_init, page_end + 1):
+    Get_URL = "https://www.nikkei.com/business/net-media/?page=" + str(page)
 
-nikkei_response = requests.get(Get_URL, headers=headers)
-soup = BeautifulSoup(nikkei_response.text, features="html.parser")
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
+    }
 
-links = soup.find_all("a", class_="fauxBlockLink_f6t5v6i")
-links_top = soup.find("a", class_="fauxBlockLink_f17mj100")
-links.insert(0, links_top)
+    nikkei_response = requests.get(Get_URL, headers=headers)
+    soup = BeautifulSoup(nikkei_response.text, features="html.parser")
 
-titles = []
-for link in links:
-    titles.append(link.get_text().replace("\u3000", ""))
+    links = soup.find_all("a", class_="fauxBlockLink_f6t5v6i")
+    links_top = soup.find("a", class_="fauxBlockLink_f17mj100")
+    links.insert(0, links_top)
 
-body_links = []
-for link in links:
-    body_links.append(link.get("href"))
+    titles = []
+    for link in links:
+        titles.append(link.get_text().replace("\u3000", ""))
 
-body_texts =[]
-for body_link in body_links:
-    get_body_link = Home_URL + body_link
+    body_links = []
+    for link in links:
+        body_links.append(link.get("href"))
 
-    body_response = requests.get(get_body_link, headers=headers)
-    body_soup = BeautifulSoup(body_response.text, features="html.parser")
+    body_texts =[]
+    for body_link in body_links:
+        get_body_link = Home_URL + body_link
 
-    try:
-        p_tag_text = body_soup.find("p", "paragraph_p18mfke4").get_text()
-    except:
-        pass
-    body_texts.append(p_tag_text)
+        body_response = requests.get(get_body_link, headers=headers)
+        body_soup = BeautifulSoup(body_response.text, features="html.parser")
 
-image_class = "image_i1obkbgm"
-img_tags = soup.find_all("img", class_=image_class)
-# print(img_tags)
+        try:
+            p_tag_text = body_soup.find("p", "paragraph_p18mfke4").get_text()
+        except:
+            pass
+        body_texts.append(p_tag_text)
 
-image_urls = []
-for img_tag in img_tags:
-    image_urls.append(img_tag.get("src"))
+    image_class = "image_i1obkbgm"
+    img_tags = soup.find_all("img", class_=image_class)
+    # print(img_tags)
 
-news_data = {
-    "title": titles,
-    "body_text": body_texts,
-    "image_url": image_urls
-}
+    image_urls = []
+    for img_tag in img_tags:
+        image_urls.append(img_tag.get("src"))
 
-df = pd.DataFrame(news_data)
-df.to_csv("nikkei_news.csv", index=None, encoding="utf-8-sig")
+    news_data = {
+        "title": titles,
+        "body_text": body_texts,
+        "image_url": image_urls
+    }
+
+    df = pd.DataFrame(news_data)
+    df.to_csv("nikkei_news.csv", index=None, encoding="utf-8-sig")
