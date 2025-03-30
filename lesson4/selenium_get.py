9c88d8caeb650aa8a4f19f64cccdbe439671eccf
Liberbell <liberbell@gmail.com>
2025-03-31 08:42:25 +0900

    selenium_get8

--- lesson4/selenium_get.py
+++ lesson4/selenium_get.py
@@ -1,5 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import configparser
+
+config_ini = configparser.ConfigParser()
+config_ini.read('config.ini', encoding='utf-8')
+
+username = config_ini['DEFAULT']['username']
+password = config_ini['DEFAULT']['password']
 
 URL1= "https://www.google.com"
 URL2 = "https://hosh-it.github.io/scraping-practice-selenium/"
@@ -14,8 +21,8 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
 
 driver.get(URL2)
-username = driver.find_element(By.ID, "username")
-password = driver.find_element(By.ID, "password")
+username_element = driver.find_element(By.ID, "username")
+password_element = driver.find_element(By.ID, "password")
 
 username.send_keys("hoshi")
 password.send_keys("pass")
