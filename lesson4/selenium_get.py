8f33fb8bab270057b5387c46a726185b0905d3a2
Liberbell <liberbell@gmail.com>
2025-03-31 08:20:10 +0900

    selenium_get5

--- lesson4/selenium_get.py
+++ lesson4/selenium_get.py
@@ -5,6 +5,11 @@ from selenium.webdriver.common.by import By
 URL1= "https://www.google.com"
 URL2 = "https://hosh-it.github.io/scraping-practice-selenium/"
 
+def get_default_chrome_options():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--no-sandbox")
+    return options
+
 options = get_default_chrome_options()
 options.add_experimental_option("detach", True)
 chrome = webdriver.Chrome(options=options)
