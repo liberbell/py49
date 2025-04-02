833dc886ec96bfd762c2d1a6259d2d3d5ea37fe1
Liberbell <liberbell@gmail.com>
2025-04-02 09:09:51 +0900

    get_api2

--- lesson5/get_api.py
+++ lesson5/get_api.py
@@ -1,9 +1,10 @@
 import configparser
 
-config_ini = configparser.ConfigParser()
+config_ini = configparser.ConfigParser(interpolation=None)
 config_ini.read('config.ini', encoding='utf-8')
-apikey = config_ini['DEFAULT']['apikey']
 password = config_ini['DEFAULT']['password']
+apikey = config_ini['DEFAULT']['apikey']
+
 print(apikey, password)
 
 endpoints = "https://newsapi.org/v2/everything"
