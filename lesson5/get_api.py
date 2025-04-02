9d83d57eb56033f8941a610ba0eedcaaa823cdde
Liberbell <liberbell@gmail.com>
2025-04-02 09:19:13 +0900

    get_api5

--- lesson5/get_api.py
+++ lesson5/get_api.py
@@ -1,11 +1,10 @@
 import configparser
+import requests
 
 config_ini = configparser.ConfigParser(interpolation=None)
 config_ini.read('config.ini', encoding='utf-8')
 password = config_ini['DEFAULT']['password']
 apikey = config_ini['DEFAULT']['apikey']
-
-print(apikey, password)
 
 endpoints = "https://newsapi.org/v2/everything"
 
@@ -13,3 +12,5 @@ params = {}
 params["apiKey"] = apikey
 params["q"] = "HPE"
 params["from"] = "2025-01-01"
+
+response = requests.get(endpoints, params=params)
