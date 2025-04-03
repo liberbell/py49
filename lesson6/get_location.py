cac69114832e3bd636372e1b261568c65a40a3ea
Liberbell <liberbell@gmail.com>
2025-04-04 08:34:50 +0900

    get_location4

--- lesson6/get_location.py
+++ lesson6/get_location.py
@@ -1,11 +1,13 @@
 import configparser
 import requests
+from bs4 import BeautifulSoup
 
 config_ini = configparser.ConfigParser(interpolation=None)
 config_ini.read('config.ini', encoding='utf-8')
 get_url1 = config_ini['DEFAULT']['Get_URL1']
 
-print(get_url1)
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
 response = requests.get(get_url1, headers=headers)
-print(response.status_code)
+
+soup = BeautifulSoup(response.text, features="html.parser")
+print(soup)
